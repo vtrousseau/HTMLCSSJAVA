65ca939c4700faa25068b77ce4126c58cb0f2c8b
vtrousseau <vtrousseau@gmail.com>
2017-01-23 23:15:38 +0000

    Add files via upload

--- mod5/js/script.js
+++ mod5/js/script.js
@@ -82,8 +82,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  buildAndShowHomeHTML,
+  homeHtml,
+  function  buildAndShowHomeHTML (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
   true);
 });
 // *** finish **
@@ -103,7 +106,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-  var chosenCategoryShortName = chooseRandomCategory(categories);
+  var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
